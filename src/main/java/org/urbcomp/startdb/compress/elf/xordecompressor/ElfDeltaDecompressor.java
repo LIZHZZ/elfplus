6f7ab41505be36a79ac8c6e9ede9315d55d4fcb2
LIIZ <1519316637@qq.com>
2023-06-10 13:39:23 +0800

    decompressor

---
+++ src/main/java/org/urbcomp/startdb/compress/elf/xordecompressor/ElfDeltaDecompressor.java
@@ -0,0 +1,80 @@
+package org.urbcomp.startdb.compress.elf.xordecompressor;
+
+import gr.aueb.delorean.chimp.InputBitStream;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ElfDeltaDecompressor {
+    private long storedVal = 0;
+    private int storedLeadingZeros = Integer.MAX_VALUE;
+    private int storedTrailingZeros = Integer.MAX_VALUE;
+    private boolean first = true;
+    private boolean endOfStream = false;
+
+    private final InputBitStream in;
+    private final static long END_SIGN = Double.doubleToLongBits(Double.NaN);
+
+    private final static short[] leadingRepresentation = {0, 8, 12, 16, 18, 20, 22, 24};
+
+    public ElfDeltaDecompressor(byte[] bs) {
+        in = new InputBitStream(bs);
+    }
+
+    public List<Double> getValues() {
+        List<Double> list = new ArrayList<>(1024);
+        Double value = readValue();
+        while (value != null) {
+            list.add(value);
+            value = readValue();
+        }
+        return list;
+    }
+
+    public InputBitStream getInputStream() {
+        return in;
+    }
+
+    /**
+     * Returns the next pair in the time series, if available.
+     *
+     * @return Pair if there's next value, null if series is done.
+     */
+    public Double readValue() {
+        try {
+            next();
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        if (endOfStream) {
+            return null;
+        }
+        return Double.longBitsToDouble(storedVal);
+    }
+
+    private void next() throws IOException {
+        if (first) {
+            first = false;
+            int trailingZeros = in.readInt(7);
+            if (trailingZeros < 64) {
+                storedVal = ((in.readLong(63 - trailingZeros) << 1) + 1) << trailingZeros;
+            } else {
+                storedVal = 0;
+            }
+            if (storedVal == END_SIGN) {
+                endOfStream = true;
+            }
+        } else {
+            nextValue();
+        }
+    }
+    private void nextValue() throws IOException{
+        long value;
+
+        int flag = in.readInt(2);
+        switch (flag){
+            case 3:
+        }
+    }
+}
